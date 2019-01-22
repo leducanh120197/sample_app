097aafb2e7024c3774e418b0f3b5224301814c5a
leducanh120197 <leducanh12011997@gmail.com>
2019-01-22 22:00:32 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,6 @@ class SessionsController < ApplicationController
 
     if user&.authenticate params[:session][:password]
       log_in user
-      params[:session][:remember_me] == Settings.string_one ? remember(user) : forget(user)
-      redirect_to user
     else
       flash.now[:danger] = t "invalid_email_password"
       render :new
@@ -17,5 +15,13 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def log_in user
+    log_in user
+    params[:session][:remember_me] == Settings.string_one ? remember(user) : forget(user)
+    redirect_back_or user
   end
 end
